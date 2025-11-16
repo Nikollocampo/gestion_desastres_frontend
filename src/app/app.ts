9aad3ef216e4a7e68f2ff9536503b909d3ce3089
JHONF2078 <jhonf2077@gmail.com>
2025-11-16 12:06:41 -0500

    feat:  estructura inicial interface

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationPage } from './navigation/pages/navigation-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/navigation/pages/navigation-page.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Footer } from "../components/footer/footer";
+import { Header } from '../components/header/header';
+import { Menu } from '../components/menu/menu';
+
+@Component({
+  selector: 'app-navigation-page',
+  imports: [Footer, Header, Menu],
+  templateUrl: './navigation-page.html',
+  styleUrl: './navigation-page.css',
+})
+export class NavigationPage {
+
+}
