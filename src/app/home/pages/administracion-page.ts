b30b22417cb8b3e043bf26dddae666e2ca8a4629
IsabelaOspina <isabela.ospinab@uqvirtual.edu.co>
2025-11-17 19:58:25 -0500

    feat: se agrego la funcional de asignar equipo en administrador

--- src/app/home/pages/administracion-page.ts
+++ src/app/home/pages/administracion-page.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/services/auth.service';
 import { BotonGenerarReporteComponent } from '../componentes/boton-generar-reporte/boton-generar-reporte.component';
 import { Desastre, DesastreService } from '../services/desastre.service';
+import { AsignarEquipoComponent } from '../componentes/asignar-equipo/asignar-equipo.component';
 
 @Component({
   selector: 'app-administracion-page',
   standalone: true,
-  imports: [BotonGenerarReporteComponent],
+  imports: [BotonGenerarReporteComponent, AsignarEquipoComponent],
   templateUrl: './administracion-page.html',
   styleUrls: ['./administracion-page.css']
 })
